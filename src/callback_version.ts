43e2b52686e1a8502c5e110f72ef01761947e444
Zmolik <62994120+Zmolik@users.noreply.github.com>
2020-12-21 01:41:05 +0100

    Add files via upload

---
+++ src/callback_version.ts
@@ -0,0 +1,59 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+import { url } from 'inspector';
+import * as path from 'path';
+const fs = require('fs');
+
+ 
+async function scrapeUrl(page_url, callback) {
+
+    const { data } = await axios.get(page_url);
+    const $ = cheerio.load(data);
+
+    const scrapedData = [];
+    console.log('start: ', page_url)
+     
+    $('.search_result_link').each((i, elem) => {      
+        const $elem = $(elem);  // each does not return html elem so I have to transform it
+        const arrayElem = $elem.text().split(' ');
+        const text = $elem.text().replace(arrayElem[0] + ' ', '');
+        scrapedData.push({
+            code: arrayElem[0],
+            preferredName: text
+        });
+    });
+    callback(scrapedData);
+}
+
+
+function getData(jsonIn, callback) {
+
+    const jsonOut = [];
+
+    for (let i = 0; i < jsonIn.length; i++) {
+        console.log(jsonIn[i].searchUrl)
+        scrapeUrl(jsonIn[i].searchUrl, (data) => {
+            jsonOut.push(
+                {
+                    ...jsonIn[i],
+                    scrapedData: data
+                }
+            )
+            if (i == jsonIn.length - 1) {
+                setTimeout( () => {callback(jsonOut)}, 50000)
+            }
+        })
+    }
+    
+}
+
+
+const fileIn = fs.readFileSync(path.join(__dirname, '..', 'json', 'diagnosis-list-SearchUrl.json'), 'utf-8');
+const jsonIn = JSON.parse(fileIn);
+
+getData(jsonIn, (jsonOut) => {
+    console.log('SAVING')
+    console.log(`saved ${jsonOut.length} elements`)
+    const fileOut = JSON.stringify(jsonOut, null, 2);
+    fs.writeFileSync(path.join(__dirname, '..', 'json', 'scrapedData.json'), fileOut);
+})
---
+++ src/index.ts
@@ -0,0 +1,122 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+import * as path from 'path';
+const fs = require('fs');
+
+
+async function createUrl(fileName) {
+    const fileIn = fs.readFileSync(path.join(__dirname, '..', 'json', fileName), 'utf-8');
+    const jsonIn = JSON.parse(fileIn);
+
+    const jsonOut = [];
+    console.log('Start creating urls.')
+    // MAIN LOOP OVER THE diagnosis-list.json
+    for (let i = 0; i < jsonIn.length; i++) {    
+
+        // creating url
+        const array: string[] = jsonIn[i].name.split(' ');
+        var url = `https://www.icd-code.de/suche/icd/recherche.html?sp=0&sp=S${array[0]}`
+        array.forEach((elem, j) => {
+            if (j > 0 && elem !== '/') {
+                url = `${url}%20${elem}`
+            }    
+        })
+        // console.log(array);
+        // console.log(url);
+        jsonOut.push(
+            {
+            ...jsonIn[i],
+            searchUrl: url
+            }
+        )
+        if (i == jsonIn.length - 1) {
+            console.log(`Created ${jsonOut.length} urls.`)
+            const fileOut = JSON.stringify(jsonOut, null, 2);
+            fs.writeFileSync(path.join(__dirname, '..', 'json', 'searchUrl.json'), fileOut);
+        }
+       
+    }
+}
+
+ 
+async function scrapeUrl(page_url, callback) {
+
+    const { data } = await axios.get(page_url);
+    const $ = cheerio.load(data);
+
+    const scrapedData = [];
+    console.log('scraping: ', page_url)
+     
+    $('.search_result_link').each((i, elem) => {      
+        const $elem = $(elem);  // each does not return html elem so I have to transform it
+        const arrayElem = $elem.text().split(' ');
+        const text = $elem.text().replace(arrayElem[0] + ' ', '');
+        scrapedData.push({
+            code: arrayElem[0],
+            preferredName: text
+        });
+    });
+    callback(scrapedData);
+}
+
+
+async function main(fileNameInput, fileNameOutput) {
+    
+    const fileIn = fs.readFileSync(path.join(__dirname, '..', 'json', fileNameInput), 'utf-8');
+    const jsonIn = JSON.parse(fileIn);
+    const jsonOut: any = [];
+
+    console.log('Urls to scrape: ', jsonIn.length)
+
+    for (let i = 1; i < jsonIn.length; i++) {
+        try {
+            await scrapeUrl(jsonIn[i].searchUrl, (data) => {
+                jsonOut.push(
+                    {
+                        ...jsonIn[i],
+                        scrapedData: data
+                    }
+                );
+                if (i == jsonIn.length - 1) {
+                    console.log(`Saved ${jsonOut.length} elements.`)
+                    const fileOut = JSON.stringify(jsonOut, null, 2);
+                    fs.writeFileSync(path.join(__dirname, '..', 'json', fileNameOutput), fileOut);
+                }
+            })
+        } catch(e) {
+            console.error(e);
+        }
+        
+    }
+ 
+}
+
+async function checkData(fileName) {
+
+    const fileIn = fs.readFileSync(path.join(__dirname, '..', 'json', fileName), 'utf-8');
+    const jsonIn = JSON.parse(fileIn);
+
+    let noCode = 0;
+    jsonIn.forEach( (elem, i) => {
+        if (elem.scrapedData.length == 0) {noCode++};
+        if (i == jsonIn.length - 1) {
+            console.log(`From ${jsonIn.length} elements, ${jsonIn.length - noCode} has at least one icd code, ${noCode} has no icd code.`)
+        }
+    })  
+}
+
+
+// createUrl('diagnosis-list.json')
+// main('searchUrl.json', 'scrapedData.json');
+// checkData('scrapedData.json')
+
+
+
+
+
+
+
+
+
+
+
